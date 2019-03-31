dcd7c0d1ecf8a44fd09df7ae0d4f40b65507e7ba
thomas delrue <thomasdelrue@gmail.com>
2019-03-31 21:47:05 +0200

    finished chapter 4

--- Chapter04/02_frozenlake_naive.py
+++ Chapter04/02_frozenlake_naive.py
@@ -13,6 +13,18 @@ BATCH_SIZE = 16
 PERCENTILE = 70
 
 
+class DiscreteOneHotWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(DiscreteOneHotWrapper, self).__init__(env)
+        assert isinstance(env.observation_space, gym.spaces.Discrete)
+        self.observation_space = gym.spaces.Box(0.0, 1.0, (env.observation_space.n,), dtype=np.float32)
+        
+    def observation(self, observation):
+        res = np.copy(self.observation_space.low)
+        res[observation] = 1.0
+        return res
+
+
 class Net(nn.Module):
     def __init__(self, obs_size, hidden_size, n_actions):
         super(Net, self).__init__()
@@ -78,7 +90,7 @@ def filter_batch(batch, percentile):
 
 
 if __name__ == '__main__':
-    env = gym.make('CartPole-v0')
+    env = DiscreteOneHotWrapper(gym.make('FrozenLake-v0'))
     # env = gym.wrappers.Monitor(env, directory="mon", force=True)
     obs_size = env.observation_space.shape[0]
     n_actions = env.action_space.n
@@ -86,7 +98,7 @@ if __name__ == '__main__':
     net = Net(obs_size, HIDDEN_SIZE, n_actions)
     objective = nn.CrossEntropyLoss()
     optimizer = optim.Adam(params=net.parameters(), lr=0.01)
-    writer = SummaryWriter()
+    writer = SummaryWriter(comment='-frozenlake-naive')
     
     for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
         obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
@@ -101,7 +113,7 @@ if __name__ == '__main__':
         writer.add_scalar('loss', loss_v.item(), iter_no)
         writer.add_scalar('reward_bound', reward_b, iter_no)
         writer.add_scalar('reward_mean', reward_m, iter_no)
-        if reward_m > 199:
+        if reward_m > 0.8:
             print('Solved!')
             break
     writer.close()
--- Chapter04/03_frozenlake_tweaked.py
+++ Chapter04/03_frozenlake_tweaked.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import gym
 import numpy as np
+import random
 from tensorboardX import SummaryWriter
 
 import torch
@@ -9,8 +10,21 @@ import torch.optim as optim
 
 
 HIDDEN_SIZE = 128
-BATCH_SIZE = 16
-PERCENTILE = 70
+BATCH_SIZE = 100
+PERCENTILE = 30
+GAMMA = 0.9
+
+
+class DiscreteOneHotWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(DiscreteOneHotWrapper, self).__init__(env)
+        assert isinstance(env.observation_space, gym.spaces.Discrete)
+        self.observation_space = gym.spaces.Box(0.0, 1.0, (env.observation_space.n,), dtype=np.float32)
+        
+    def observation(self, observation):
+        res = np.copy(self.observation_space.low)
+        res[observation] = 1.0
+        return res
 
 
 class Net(nn.Module):
@@ -60,48 +74,55 @@ def iterate_batches(env, net, batch_size):
         
 
 def filter_batch(batch, percentile):
-    rewards = list(map(lambda s: s.reward, batch))
-    reward_bound = np.percentile(rewards, percentile)
-    reward_mean = float(np.mean(rewards))
-    
+    disc_rewards = list(map(lambda s: s.reward * (GAMMA ** len(s.steps)), batch))
+    reward_bound = np.percentile(disc_rewards, percentile)
+        
     train_obs = []
     train_act = []
-    for example in batch:
-        if example.reward < reward_bound:
-            continue
-        train_obs.extend(map(lambda step: step.observation, example.steps))
-        train_act.extend(map(lambda step: step.action, example.steps))
+    elite_batch = []
+    for example, discounted_reward in zip(batch, disc_rewards):
+        if discounted_reward > reward_bound:
+            train_obs.extend(map(lambda step: step.observation, example.steps))
+            train_act.extend(map(lambda step: step.action, example.steps))
+            elite_batch.append(example)
     
-    train_obs_v = torch.FloatTensor(train_obs)
-    train_act_v = torch.LongTensor(train_act)
-    return train_obs_v, train_act_v, reward_bound, reward_mean
+    return elite_batch, train_obs, train_act, reward_bound
 
 
 if __name__ == '__main__':
-    env = gym.make('CartPole-v0')
+    random.seed(12345)
+    env = DiscreteOneHotWrapper(gym.make('FrozenLake-v0'))
     # env = gym.wrappers.Monitor(env, directory="mon", force=True)
     obs_size = env.observation_space.shape[0]
     n_actions = env.action_space.n
     
     net = Net(obs_size, HIDDEN_SIZE, n_actions)
     objective = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(params=net.parameters(), lr=0.01)
-    writer = SummaryWriter()
+    optimizer = optim.Adam(params=net.parameters(), lr=0.001)
+    writer = SummaryWriter(comment='-frozenlake-naive')
     
+    full_batch = []
     for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
-        obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
+        reward_mean = float(np.mean(list(map(lambda s: s.reward, batch))))
+        full_batch, obs, acts, reward_bound = filter_batch(full_batch + batch, PERCENTILE)
+        if not full_batch:
+            continue
+        obs_v = torch.FloatTensor(obs)
+        acts_v = torch.LongTensor(acts)
+        full_batch = full_batch[-500:]
+        
         optimizer.zero_grad()
         action_scores_v = net(obs_v)
         loss_v = objective(action_scores_v, acts_v)
         loss_v.backward()
         optimizer.step()
         
-        print('%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f' %
-              (iter_no, loss_v.item(), reward_m, reward_b))
+        print('%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f, batch=%d' %
+              (iter_no, loss_v.item(), reward_mean, reward_bound, len(full_batch)))
         writer.add_scalar('loss', loss_v.item(), iter_no)
-        writer.add_scalar('reward_bound', reward_b, iter_no)
-        writer.add_scalar('reward_mean', reward_m, iter_no)
-        if reward_m > 199:
+        writer.add_scalar('reward_bound', reward_bound, iter_no)
+        writer.add_scalar('reward_mean', reward_mean, iter_no)
+        if reward_mean > 0.8:
             print('Solved!')
             break
     writer.close()
